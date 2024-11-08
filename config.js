a76e61a74546be5d287c4b6e2c0cd469fc0056b9
Aji Setiawan <106381048+ajisetiawan716@users.noreply.github.com>
2024-11-08 08:44:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
  * https://beta.developer.spotify.com/dashboard
  */
 
-const client_id = 'YOUR_CLIENT_ID';
-const redirect_uri = 'YOUR_CALLBACK_URL';
-const client_secret = 'YOUR_CLIENT_SECRET';
+const client_id = '213841b3fb1b487188c947dc39a745da';
+const redirect_uri = 'http://localhost:3000';
+const client_secret = '3f8e13d7dd0248e0ac15c703fc0be870';
